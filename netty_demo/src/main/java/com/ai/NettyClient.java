d9cc706e8850c2b0edb91568b9e57508d50479ad
wangjinliang1991 <wjl31802@126.com>
2023-03-25 21:08:10 +0800

    netty client write message to server

--- netty_demo/src/main/java/com/ai/NettyClient.java
+++ netty_demo/src/main/java/com/ai/NettyClient.java
@@ -1,12 +1,16 @@
 package com.ai;
 
 import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.Channel;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
+
+import java.nio.charset.StandardCharsets;
 
 public class NettyClient {
     public static void main(String[] args) {
@@ -27,6 +31,13 @@ public class NettyClient {
                         }
                     });
             ChannelFuture future = bootstrap.bind(host, port).sync();
+
+            Channel channel = future.channel();
+            ByteBuf buffer = channel.alloc().buffer();
+            String msg = "hello netty server, I am netty client!";
+            buffer.writeBytes(msg.getBytes(StandardCharsets.UTF_8));
+            channel.writeAndFlush(buffer);
+
             future.channel().closeFuture().sync();
         } catch (InterruptedException e) {
             e.printStackTrace();
